a205a66d29f5f6710a45ed8f75f0f07bd6e92209
bozoputer <e@kathuria.dev>
2024-09-04 14:58:22 -0400

    Add component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
+<script>
+  import Comp1 from '../components/Comp1.svelte';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Comp1 />
